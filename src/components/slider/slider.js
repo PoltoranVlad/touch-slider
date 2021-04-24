401a5b964c072b56d2d497b9cec37a9b897d34b1
vlad <poltor.vlad@mail.ru>
2021-04-24 19:25:37 +0300

    removed useState

--- src/components/slider/slider.js
+++ src/components/slider/slider.js
@@ -1,70 +1,68 @@
-import React, {useState} from 'react';
+import React, {Component} from 'react';
 import './slider.css'
 import SliderArray from "./slides/slides";
 
-const Slider = () => {
-
-    const [x, setX] = useState(0)
-    const arrLength = SliderArray.length;
-    const step = 22.5;
-    let isInitial = false;
-
-    const prev = () => {
-        setX(x + step)
-        if (x < step * (-arrLength / 2)) {
-            setX(0)
-        } else if (x >= 0) {
-            setX(step * (-arrLength / 2))
+export default class Slider extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            x: 0,
+            // currentPos: 0,
+            initialPos: 0,
         }
+
+        this.bNext = this.bNext.bind(this);
+        this.tMove = this.tMove.bind(this);
+        this.tStart = this.tStart.bind(this);
+
     }
-    const next = () => {
-        setX(x - step)
-        if (x <= step * (-arrLength / 2)) {
-            setX(0)
-        } else if (x > 0) {
-            setX(step * (-arrLength / 2))
-        }
+
+    bNext(){
+        let _step = 222;
+        this.setState(({x}) => ({
+            x: x - _step
+        }))
     }
-    let moving = false,
-        initialPos = 0;
-    const tMove = (e) => {
-        if (moving) {
-            let currentPos = e.touches[0].clientX;
-            let dif = currentPos - initialPos;
-            setX(dif)
-        }
-    }
-    const tStart = (e) => {
-        moving = true;
-        initialPos = e.touches[0].clientX;
-    }
-    const tEnd = (e) => {
-        moving = false;
+
+    tStart(e){
+        this.setState(({initialPos}) => ({
+            initialPos: Math.round(e.touches[0].clientX)
+        }))
+        console.log('START POS - '+this.state.initialPos)
     }
 
 
-    return (
-        <div className="outer track">
-            <div className="inner track"
-                 onTouchStart={tStart}
-                 onTouchMoveCapture={tMove}
-                 onTouchEnd={tEnd}
-            >
-                {SliderArray.map((slide, i) => {
-                    return <img
-                        key={i}
-                        src={slide.path}
-                        alt={'=('}
-                        className='colors track'
-                        style={{transform: `translateX(${x}px)`}}
-                    />
-                })}
-                <button className="button prev" onClick={prev}/>
-                <button className="button next" onClick={next}/>
+    tMove(e) {
+        let currentPos = Math.round(e.touches[0].clientX)
+        let dif = currentPos - this.state.initialPos
+        this.setState(({x}) => ({
+            x: dif
+        }))
+        console.log((currentPos))
+    }
+
+    render() {
+        return (
+            <div className="outer track">
+                <div className="inner track"
+                    onTouchStart={this.tStart}
+                    onTouchMove={this.tMove}
+                    // onTouchEnd={}
+                >
+                    {SliderArray.map((slide, i) => {
+                        return <img
+                            key={i}
+                            src={slide.path}
+                            alt={'=('}
+                            className='colors track'
+                            style={{transform: `translateX(${this.state.x}px)`}}
+                        />
+                    })}
+                    <button className="button prev" onClick={this.bNext}/>
+                    <button className="button next" onClick={this.bNext}/>
+                </div>
             </div>
-        </div>
-    )
-
+        )
+    }
 }
-export default Slider
 
